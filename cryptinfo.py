348b68a09ff9916b389881db30b913b5d9ba6ea5
leviOnNet <65132823+leviOnNet@users.noreply.github.com>
2022-01-03 17:34:21 +0200

    Update cryptinfo.py

--- cryptinfo.py
+++ cryptinfo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from rates import rate
 from binance import AsyncClient
-from binance.client import client
+from binance.client import Client
 import config1
 from pandas.core.frame import DataFrame
 from datetime import datetime
